c47e9fabc03d26b8699206caa85b22819afe832e
Abhishek Amralkar <abhishekamralkar@gmail.com>
2021-04-03 22:11:05 +0530

    adding th variable soutions

---
+++ variables/02-declarations/exercises/06-with-bits/main.go
@@ -0,0 +1,34 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var i16 int16
+	var i32 int32
+	var i64 int64
+	var f32 float32
+	var f64 float64
+	var c64 complex64
+	var c128 complex128
+	var b bool
+	var s string
+
+	fmt.Println(i16)
+	fmt.Println(i32)
+	fmt.Println(i64)
+	fmt.Println(f32)
+	fmt.Println(f64)
+	fmt.Println(c64)
+	fmt.Println(c128)
+	fmt.Println(b)
+	fmt.Println(s)
+}
