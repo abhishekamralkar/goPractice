004dce17b5aae8cf8ec982636fd6115b85fb26c6
Abhishek Amralkar <abhishekamralkar@gmail.com>
2021-04-04 21:10:35 +0530

    adding exercise 9 and 10

---
+++ printf/exercises/10-print-your-fullname/main.go
@@ -0,0 +1,22 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	name, lastname := os.Args[1], os.Args[2]
+
+	msg := "Your name is %s and your lastname is %s.\n"
+	fmt.Printf(msg, name, lastname)
+}
