8d6415ccd4ce6525e6e2cbdf94be82719abf121f
Abhishek Amralkar <abhishekamralkar@gmail.com>
2021-04-04 20:32:25 +0530

    adding the first 2 excercise of printf

---
+++ printf/exercises/02-print-your-name-and-lastname/main.go
@@ -0,0 +1,20 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import "fmt"
+
+func main() {
+	// BONUS: Use a variable for the format specifier
+
+	firstName := "Anay"
+	lastName := "Amralkar"
+
+	fmt.Printf("My name is %s and my lastname is %s.\n", firstName, lastName)
+}
