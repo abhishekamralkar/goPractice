651ac2964614224e39fd7bc0d18ef2a01034f919
Abhishek Amralkar <abhishekamralkar@gmail.com>
2021-04-18 17:16:04 +0530

    add loop exercises

---
+++ loops/exercises/01-sum-the-numbers/main.go
@@ -0,0 +1,19 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import "fmt"
+
+func main() {
+	sum := 0
+	for i := 0; i <= 10; i++ {
+		sum += i
+	}
+	fmt.Println("Sum:", sum)
+}
